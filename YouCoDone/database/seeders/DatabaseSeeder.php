59ed36825ef62611f7f195b959c7bf4a22230928
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-02-03 17:11:09 +0100

    feat : update database seeder

--- YouCoDone/database/seeders/DatabaseSeeder.php
+++ YouCoDone/database/seeders/DatabaseSeeder.php
@@ -3,23 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Restaurant;
+use App\Models\Menu;
+use App\Models\MenuItem;
+use App\Models\Favorite;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Restaurant::factory(5)
+            ->has(
+                Menu::factory(2)
+                    ->has(MenuItem::factory(4))
+            )
+            ->create();
+
+        Favorite::factory(10)->create();
     }
 }
