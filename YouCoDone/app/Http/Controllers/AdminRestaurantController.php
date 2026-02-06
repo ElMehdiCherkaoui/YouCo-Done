0a385f3fd84ee958c68a990024bc0da17ac4e49e
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-02-06 20:18:27 +0100

    feat : add all function Controller for restaurant

--- YouCoDone/app/Http/Controllers/AdminRestaurantController.php
+++ YouCoDone/app/Http/Controllers/AdminRestaurantController.php
@@ -11,7 +11,8 @@ class AdminRestaurantController extends Controller
 
     public function index()
     {
-        $restaurants = Restaurant::with('user')->get();
+        $restaurants = Restaurant::with('user')->paginate(10)
+            ->withQueryString();
 
         $totalRestaurants = Restaurant::count();
         $activeRestaurants = Restaurant::where('isActive', true)->count();
