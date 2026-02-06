82d47def5575429d43362328e665e19497f09e74
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-02-06 10:21:27 +0100

    feat : add All factorys needed

--- YouCoDone/database/factories/RestaurantFactory.php
+++ YouCoDone/database/factories/RestaurantFactory.php
@@ -11,14 +11,22 @@ class RestaurantFactory extends Factory
     {
         return [
             'name' => $this->faker->company,
+            'description' => $this->faker->sentence(10),
+            'number' => $this->faker->phoneNumber(),
+            'email' => $this->faker->email(),
             'image' => $this->faker->imageUrl(640, 480, 'food'),
             'city' => $this->faker->city,
             'address' => $this->faker->address,
             'cuisine_type' => $this->faker->randomElement([
-                'Italian', 'Moroccan', 'French', 'Japanese', 'Mexican'
+                'Italian',
+                'Moroccan',
+                'French',
+                'Japanese',
+                'Mexican'
             ]),
             'capacity' => $this->faker->numberBetween(20, 200),
             'opening_hours' => '09:00 - 23:00',
+            'isActive' => $this->faker->boolean(70),
             'user_id' => User::factory(),
         ];
     }
