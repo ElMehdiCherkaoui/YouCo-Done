49825f1808bcf117a89031844c5d83e9307348ed
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-02-06 10:18:48 +0100

    add aminrestaurant controller

---
+++ YouCoDone/app/Http/Controllers/AdminRestaurantController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Restaurant;
+
+class AdminRestaurantController extends Controller
+{
+
+    public function index()
+    {
+        $restaurants = Restaurant::with('user')->get();
+
+        $totalRestaurants = Restaurant::count();
+        $activeRestaurants = Restaurant::where('isActive', true)->count();
+
+        return view('admin.restaurants', compact(
+            'restaurants',
+            'totalRestaurants',
+            'activeRestaurants',
+        ));
+    }
+
+    public function destroy(string $id)
+    {
+        $restaurant = Restaurant::findOrFail($id);
+        $restaurant->delete();
+
+        return redirect()->route('admin.restaurants');
+    }
+}
