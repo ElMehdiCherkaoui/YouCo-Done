81dd398f33d4a1e14bcbbfe0e711cd6a9be30137
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-02-06 10:24:28 +0100

    feat add all routes needed

--- YouCoDone/routes/web.php
+++ YouCoDone/routes/web.php
@@ -3,13 +3,16 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Types\Relations\Role;
+use App\Http\Controllers\clientController;
+use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\AdminRestaurantController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\FavouritController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+
 Route::get('/dashboard', function () {
     $user = auth()->user();
-
     if ($user->hasRole('admin')) {
         return view('admin.dashboard');
     } elseif ($user->hasRole('client')) {
@@ -17,43 +20,42 @@ Route::get('/dashboard', function () {
     } elseif ($user->hasRole('restaurant')) {
         return view('restaurateur.dashboard');
     }
-
     abort(403);
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/client/restaurants', function () {
-    return view('client.restaurantLists');
-})->middleware(['auth'])->name('client.restaurant');
+Route::get('/client/restaurants', [clientController::class, 'index'])->middleware(['auth'])->name('client.restaurant');
 
-Route::get('/client/restaurants/details', function () {
-    return view('client.restaurantDetails');
-})->middleware(['auth'])->name('client.restaurantDetails');
+Route::get('/client/restaurants/details', function () {return view('client.restaurantDetails');})->middleware(['auth'])->name('client.restaurantDetails');
 
+Route::post('/client/{restaurant}/favorite', [FavouritController::class, 'store'])->middleware('auth')->name('restaurant.favorite.store');
 
-Route::get('/restaurateur/restaurants', function () {
-    return view('restaurateur.restaurants');
-})->middleware(['auth'])->name('restaurateur.restaurants');
+Route::delete('/client/{restaurant}/favorite', [FavouritController::class, 'destroy'])->middleware('auth')->name('restaurant.favorite.destroy');
 
-Route::get('/restaurateur/restaurants/create', function () {
-    return view('restaurateur.create');
-})->middleware(['auth'])->name('restaurateur.create');
+Route::get('/client/restaurants/{restaurant}',[clientController::class, 'show'])->middleware(['auth'])->name('client.restaurant.show');
 
-Route::get('/restaurateur/restaurants/edit', function () {
-    return view('restaurateur.edit');
-})->middleware(['auth'])->name('restaurateur.edit');
+Route::get('/restaurateur/restaurants', [RestaurantController::class, 'index'])->middleware(['auth'])->name('restaurateur.restaurants');
 
-Route::get('/admin/restaurants', function () {
-    return view('admin.restaurants');
-})->middleware(['auth'])->name('admin.restaurants');
+Route::get('/restaurateur/restaurants/create', [RestaurantController::class, 'create'])->middleware(['auth'])->name('restaurateur.create');
 
-Route::get('/admin/users', function () {
-    return view('admin.users');
-})->middleware(['auth'])->name('admin.users');
+Route::get('/restaurateur/restaurants/edit/{restaurant}',  [RestaurantController::class, 'edit'])->middleware(['auth'])->name('restaurateur.edit');
 
+Route::post('/restaurateur/restaurants/{restaurant}',[RestaurantController::class, 'update'])->middleware(['auth'])->name('restaurateur.restaurant.update');
 
+Route::get('/restaurateur/restaurant/{restaurant}/menu/create',[MenuController::class, 'create'])->name('restaurateur.menu.create');
 
+Route::post('/restaurateur/restaurant/{restaurant}/menu',[MenuController::class, 'store'])->name('restaurateur.menu.store');
 
+Route::delete('/restaurateur/menu/{id}',[MenuController::class, 'destroy'])->name('restaurateur.menu.destroy');
 
+Route::get('/restaurateur/restaurant/{id}',[RestaurantController::class, 'show'])->middleware(['auth'])->name('restaurateur.restaurant.show');
+
+Route::post('/restaurateur/restaurant/Store', [RestaurantController::class, 'store'])->middleware(['auth'])->name('restaurateur.store');
+
+Route::get('/admin/restaurants', [AdminRestaurantController::class, 'index'])->middleware(['auth'])->name('admin.restaurants');
+
+Route::get('/admin/users', function () {return view('admin.users');})->middleware(['auth'])->name('admin.users');
+
+Route::delete('/admin/restaurants/{restaurant}', [RestaurantController::class, 'destroy'])->middleware(['auth'])->name('restaurants.destroy');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
